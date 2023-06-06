1c028bffd29f44995b6dfd1662c70e8c778b8e46
Atri2333 <AtriKawaii@outlook.com>
2023-06-06 21:06:00 +0800

    finished torch dataset and dataloader learning

--- python/test_tb.py
+++ python/test_tb.py
@@ -1,9 +1,15 @@
 from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+from PIL import Image
 
 writer = SummaryWriter("logs")
+image_path = r"hymenoptera_data/train/bees_image/16838648_415acd9e3f.jpg"
+img_PIL = Image.open(image_path)
+# img_PIL.show()
+img_array = np.array(img_PIL)
+print(type(img_array))
+print(img_array.shape)
 
-# writer.add_image()
-for i in range(100):
-    writer.add_scalar("y=2x", 3*i, i)
+writer.add_image("fuck", img_array, 1, dataformats="HWC")
 
 writer.close()
---
+++ python/useful_transforms.py
@@ -0,0 +1,41 @@
+from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+
+writer = SummaryWriter("logs")
+img = Image.open("Image/pdc_000000.png")
+print(img)
+
+trans_totensor = transforms.ToTensor()
+img_tensor = trans_totensor(img)
+writer.add_image("ToTensor", img_tensor)
+
+print(img_tensor.shape)
+print(img_tensor[1][0][0])
+trans_norm = transforms.Normalize([1,1,1,1],[1,1,1,1])
+img_norm = trans_norm(img_tensor)
+print(img_norm[1][0][0])
+writer.add_image("Normalize", img_norm, 1)
+
+print(img.size)
+trans_resize = transforms.Resize((512,512))
+img_resize = trans_resize(img)
+img_resize = trans_totensor(img_resize)
+writer.add_image("Resize", img_resize, 0)
+print(img_resize)
+
+trans_resize_2 = transforms.Resize(512)
+trans_compose = transforms.Compose([trans_resize_2,
+                                    trans_totensor])
+img_resize_2 = trans_compose(img)
+writer.add_image("Resize", img_resize_2, 1)
+
+trans_random = transforms.RandomCrop((500,1000))
+trans_compose_2 = transforms.Compose([trans_random,
+                                      trans_totensor])
+for i in range(10):
+    img_crop = trans_compose_2(img)
+    writer.add_image("RandomCropHW", img_crop, i)
+
+
+writer.close()
